340e57d15ae891177d2d3e6fd8e47ef052ddf2b5
ngkwdsk <nagakawadai@gmail.com>
2024-05-18 19:10:21 +0900

    パスワードのバリデーションの修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,7 +4,7 @@ FactoryBot.define do
   factory :user do
     nickname { Faker::Name.name }
     email { Faker::Internet.email }
-    password { Faker::Internet.password(min_length: 6) }
+    password { '1a' + Faker::Internet.password(min_length: 6) }
     password_confirmation { password }
     last_name { person.last.kanji }
     first_name { person.first.kanji }
