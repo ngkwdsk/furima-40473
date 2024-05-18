61b265b454df90d3135ef8fa3b92eb997b643628
ngkwdsk <nagakawadai@gmail.com>
2024-05-18 17:26:14 +0900

    メールアドレスの一意性の設定

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,19 +9,49 @@ RSpec.describe User, type: :model do
     context '新規登録できる場合' do
       it "nickname、email、password、password_confirmation、last_name、first_name、last_name_kana、first_name_kana、birthday
       が存在すれば登録できる" do
-
       end
     end
     context '新規登録できない場合' do
-      it "nicknameが空では登録できない" do
-
+      it 'nicknameが空では登録できない' do
+        @user.nickname = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Name can't be blank")
       end
-      it "emailが空では登録できない" do
-
+      it 'emailが空では登録できない' do
+        @user.email = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Email can't be blank")
       end
-      it "passwordが空では登録できない" do
-
+      it 'emailが'
       end
+      it 'passwordが空では登録できない' do
+        @user.password = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password can't be blank")
+      end
+
+      it 'passwordが5文字以下では登録できない' do
+        @user.password = '1234a'
+        @user.password_confirmation = '1234a'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
+      end
+
+      it 'passwordが129文字以上では登録できない' do
+        @user.password = Faker::Internet.password(min_length: 129, max_length: 150)
+        @user.password_confirmation = @user.password
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Password is too long (maximum is 128 characters)')
+      end
+
+      it 'passwordとpassword_confirmationが不一致では登録できない' do
+        @user.password = '12345a'
+        @user.password_confirmation = '12345ab'
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
+      end
+
+
     end
   end
 end
