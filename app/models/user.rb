f0ffa0a27028b74d68838a00d5fddcaddbfc284f
ngkwdsk <nagakawadai@gmail.com>
2024-05-18 17:14:46 +0900

    本人情報のバリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i
   validates_format_of :password, with: PASSWORD_REGEX, message: 'must be alphanumeric characters'
 
   validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'must be full-width characters' } do
+    validates :last_name
+    validates :first_name
+    validates :last_name_kana
+    validates :first_name_kana
+  end
   validates :birthday, presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  before do
+    @user = FactoryBot.build(:user)
+  end
+
+  describe 'ユーザー新規登録' do
+    context '新規登録できる場合' do
+      it "nickname、email、password、password_confirmation、last_name、first_name、last_name_kana、first_name_kana、birthday
+      が存在すれば登録できる" do
+
+      end
+    end
+    context '新規登録できない場合' do
+      it "nicknameが空では登録できない" do
+
+      end
+      it "emailが空では登録できない" do
+
+      end
+      it "passwordが空では登録できない" do
+
+      end
+    end
+  end
 end
