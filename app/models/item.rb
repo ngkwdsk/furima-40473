c600188bccc3c92679cd277272e4e8109b679fb5
ngkwdsk <nagakawadai@gmail.com>
2024-05-21 00:00:57 +0900

    商品の詳細、発送についてのActiveHushの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,4 +7,8 @@ class Item < ApplicationRecord
 
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
+  belongs_to :condition
+  belongs_to :postage
+  belongs_to :region
+  belongs_to :ship_date
 end
