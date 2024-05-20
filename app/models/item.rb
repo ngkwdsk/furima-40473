a8c9bef02b2da9b1f2ecbc66601a6453ba565c31
ngkwdsk <nagakawadai@gmail.com>
2024-05-20 20:10:45 +0900

    itemモデルのバリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
+  belongs_to :user
+  has_one_attached :image
 
-	belongs_to :user
-	has_one_attached :image
-
+	validates :name, :caption, :category_id, :condition_id, :postage_id, :region_id, :ship_date_id, :price, :image, presence: true	
 end
