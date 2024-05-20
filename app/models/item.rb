fa123d25767ee026f7f4b53b002bf90a5023d3cc
ngkwdsk <nagakawadai@gmail.com>
2024-05-20 21:43:19 +0900

    カテゴリーの記述とアイソレーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,5 +2,9 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
-	validates :name, :caption, :category_id, :condition_id, :postage_id, :region_id, :ship_date_id, :price, :image, presence: true	
+  validates :name, :caption, :category_id, :condition_id, :postage_id, :region_id, :ship_date_id, :price, :image, presence: true
+  validates :category_id, :condition_id, :postage_id, :region_id, :ship_date_id, numericality: { other_than: 1, message: "can't be blank" }
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
 end
