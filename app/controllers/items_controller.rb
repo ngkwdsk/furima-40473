b6e1e8f74752c696beac5dca3e8b189b5f8267f9
ngkwdsk <nagakawadai@gmail.com>
2024-05-21 21:25:33 +0900

    Createアクションの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,19 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.create(item_params)
+    redirect_to root_path
+  end
+end
+
+private
+
+def item_params
+  params.require(:item).permit(:name, :caption, :category_id, :condition_id, :postage_id, :region_id, :ship_date_id, :price, :image).merge(user_id: current_user.id)
 end
