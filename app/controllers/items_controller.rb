3c2881fa4e1c45d61b911cf6261f5b5a1ab12a6b
ngkwdsk <nagakawadai@gmail.com>
2024-05-25 14:29:42 +0900

    コントローラーの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,6 +23,22 @@ class ItemsController < ApplicationController
     end
   end
 
+  def edit
+    @item = Item.find(parames[:id])
+    unless @item.user == current_user
+      redirect_to item_path
+    end
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to root_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def item_params
