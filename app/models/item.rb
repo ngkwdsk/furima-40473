73f8110198ea5613e7d87f1e64def0b92f62c6f7
ngkwdsk <nagakawadai@gmail.com>
2024-05-31 21:36:02 +0900

    アイソレーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,7 @@
 class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
+  has_one :purchase
 
   validates :name, :caption, :category_id, :condition_id, :postage_id, :region_id, :ship_date_id, :price, :image, presence: true
   validates :category_id, :condition_id, :postage_id, :region_id, :ship_date_id, numericality: { other_than: 1, message: "can't be blank" }
