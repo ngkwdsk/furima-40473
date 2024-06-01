67ab5f8388c93ac34a0bdda4cabb04b86ae1553a
ngkwdsk <nagakawadai@gmail.com>
2024-06-01 12:33:03 +0900

    orderコントローラーのデータ保存の記述

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,5 +3,22 @@ class OrdersController < ApplicationController
 
   def index
     @item = Item.find(params[:item_id])
+		@purchase_address = PurchaseAddress.new
   end
+
+	def create
+		@purchase_address = PurchaseAddress.new(order_params)
+		if @purchase_address.valid?
+			@purchase_address.save
+			redirect_to root_path
+		else
+			render :new, status: :unprocessable_entity
+		end
+	end
+
+	private
+
+	def order_params
+		params.require(:purchase_address).permit(:post_code, :region_id, :city, :block, :building, :phone, :purchase_id)
+	end
 end
