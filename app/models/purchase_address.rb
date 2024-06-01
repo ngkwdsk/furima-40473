feb4f88b7ac7798369b98782836fc534163bcc89
ngkwdsk <nagakawadai@gmail.com>
2024-06-01 16:51:42 +0900

    PurchaseAddressモデルの実装

--- app/models/purchase_address.rb
+++ app/models/purchase_address.rb
@@ -1,6 +1,6 @@
 class PurchaseAddress
   include ActiveModel::Model
-  attr_accessor :post_code, :region_id, :city, :block, :building, :phone, :item_id, :user_id 
+  attr_accessor :post_code, :region_id, :city, :block, :building, :phone, :item_id, :user_id, :token
 
   with_options presence: true do
     validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Enter it as follows (e.g. 123-4567)' }
@@ -8,12 +8,11 @@ class PurchaseAddress
     validates :city, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'must be full-width characters' }
     validates :block
     validates :phone, format: {with: /\A0\d{9,10}\z/, message: 'is invalid. Enter it as follows (e.g. 012345678)' }
-    validates :item_id
-    validates :user_id
+    validates :token
   end
 
   def save
     purchase = Purchase.create(item_id: item_id, user_id: user_id)
-    Address.create(post_code: post_code, region_id: region_id, city: city, block: block, building: building, phone: phone, purchase_id: purchase_id )
+    Address.create(post_code: post_code, region_id: region_id, city: city, block: block, building: building, phone: phone, purchase_id: purchase.id )
   end
 end
